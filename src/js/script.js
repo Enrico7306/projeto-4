eb09fce56f6e9bb4e211e7b697cdeffc98113c7b
Laboratório FIAP <enricobborges06@gmail.com>
2025-05-09 10:21:42 -0300

    adicionando algumas coisas

--- src/js/script.js
+++ src/js/script.js
@@ -22,3 +22,21 @@ function slideShow(){
 
 }
 slideShow();
+const toggleBtn = document.querySelector(".side-bar-");
+const closeBtn = document.querySelector(".close");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  // if (sidebar.classList.contains("show-sidebar")) {
+  //   sidebar.classList.remove("show-sidebar");
+  // } else {
+  //   sidebar.classList.add("show-sidebar");
+  // }
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+
+
